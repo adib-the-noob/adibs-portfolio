a139169f1600041a9e974abbcf68929213a4b7c2
adib-the-noob <mdadib550@gmail.com>
2024-11-24 16:52:04 +0600

    navbar icons added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import { FaFacebook, FaXTwitter } from "react-icons/fa6";
+import logo from "../assets/adibs-icon.webp"
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <a href="/" aria-label="Home">
+                    <img src={logo} className="" width={50} height={33} alt="Logo"/>
+                </a>
+            </div>
+
+            <div className="mx-8 flex items-center justify-center gap-4 text-2xl">
+                <a 
+                    href="https://www.linkedin.com/in/adib550/"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="LinkedIn"
+                >
+                    <FaLinkedin />
+                </a>
+                <a 
+                    href="http://github.com/adib-the-noob"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="GitHub"
+                >
+                    <FaGithub />
+                </a>
+                <a 
+                    href="https://www.instagram.com/adib_the_noob/"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="Instagram"
+                >
+                    <FaInstagram />
+                </a>
+                <a 
+                    href="https://x.com/Adib70980567/"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="Twitter"
+                >
+                    <FaXTwitter />
+                </a>
+                <a 
+                    href="https://www.facebook.com/noob.adib/"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="Facebook"
+                >
+                    <FaFacebook />
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
